0d7a3536404b4da67e1f4052e268b69467323b28
neilpang <github@byneil.com>
2017-03-25 14:11:53 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
 
 sudo apt-get install -qqy openbox obmenu obconf vnc4server xvt fbpanel pcmanfm ttf-wqy-zenhei firefox
 
 
+vncserver :1
+vncserver -kill :1
 
-sed  -i "s/x-window-manager/openbox-session/" "~/.vnc/xstartup"
+sed  -i "s/x-window-manager/openbox-session/" ~/.vnc/xstartup
 
 mkdir -p ~/.config/openbox/
 
@@ -14,4 +17,5 @@ pcmanfm &
 firefox &" > ~/.config/openbox/autostart.sh
 
 
+vncserver :1
 
